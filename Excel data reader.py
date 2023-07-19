e6ec1fa2d07b2258a80df93a7a24ba94af5c84a8
Divyesh Gadhiya <139940901+Divyesh0G@users.noreply.github.com>
2023-07-19 03:30:17 -0400

    Update Excel data reader.py

--- Excel data reader.py
+++ Excel data reader.py
@@ -4,43 +4,36 @@ from openpyxl.worksheet.table import Table, TableStyleInfo
 
 from tester import infofill
 
-
 def excel_tableprint(infofill):
-    # spl = str(datetime.datetime.now()).split(':')
-    shtname = str(datetime.date.today())#spl[0] #+ '-' + spl[1]
+    # Step 1: Set the sheet name based on the current date
+    shtname = str(datetime.date.today())
 
-
-    # lode excel file
+    # Step 2: Load the Excel file
     wb = openpyxl.load_workbook('hello.xlsx')
 
-
-    t=wb.get_active_sheet
-
-
-    # creat a sheet loop
-
-    # if t == shtname:
-    #     print(t)
-    #     print("its a ,atch")
-    # else:
-    #     wb.create_sheet(shtname)
-    #     wb.active=-1
-    #     print("here")
-
-    print(wb.sheetnames)
-    print(wb.active)
+    # Step 3: Get the active sheet
     ws = wb.active
 
-    ws.append([" Entry No. ", " Flat No. ", " CustomerID ", " Date ", " Amount "])
+    # Step 4: Append the table headers to the sheet
+    ws.append(["Entry No.", "Flat No.", "CustomerID", "Date", "Amount"])
+
+    # Step 5: Append the data from infofill to the sheet
     for column in infofill:
         ws.append(column)
 
+    # Step 6: Create a table and apply formatting
     tab = Table(displayName="Table1", ref="A1:E5")
-    style = TableStyleInfo(name="TableStyleMedium9", showFirstColumn=False,
-                           showLastColumn=False, showRowStripes=True, showColumnStripes=True)
+    style = TableStyleInfo(
+        name="TableStyleMedium9",
+        showFirstColumn=False,
+        showLastColumn=False,
+        showRowStripes=True,
+        showColumnStripes=True
+    )
     tab.tableStyleInfo = style
     ws.add_table(tab)
 
+    # Step 7: Save the modified Excel file
     wb.save('hello.xlsx')
 
 if __name__ == '__main__':
